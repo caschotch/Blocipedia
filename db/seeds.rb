7f61eff27d77ccb0edde16dd0fc50a1ccd8952b8
Craig A Schotch II <caschotchii@gmail.com>
2017-01-07 08:43:14 -0800

    Refactored faker data entry

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,17 +14,18 @@ require 'faker'
     password: Faker::Internet.password(8,16)
   )
 end
+users = User.all
 
 50.times do
   Wiki.create!(
     title: Faker::Lorem.word,
     body: Faker::Lorem.paragraph,
-    user_id: Faker::Number.between(1,15)
+    user: users.sample
   )
 end
 
 wikis = Wiki.all
-users = User.all
+
 
 
 puts "Seed finished"
